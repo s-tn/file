c6e556075cca306b01d143c0a7738661ba22e46f
Ender <78042486+s-tn@users.noreply.github.com>
2024-02-05 17:37:44 -0500

    final commit hopefully

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
 const express = require('express');
 const app = express();
 const multer = require('multer');
+const fs = require("node:fs/promises");
 
-const upload = multer({
-    dest: 'public/uploads/'
-});
+try {
+    await fs.readdir(__dirname + '/public/uploads');
+} catch {
+    await fs.mkdir(__dirname + '/public/uploads');
+}
+
+let currentDir = parseInt((await fs.readdir(__dirname + '/public/uploads')).pop() || '0');
+let dest = __dirname + `/public/uploads/${currentDir}/`;
+
+let a = {
+    _upload: multer({
+        dest
+    }),
+    get upload() {
+        return this._upload;
+    }
+}
+
+reloadDb = (d) => (dest = d, a._upload = multer({ dest }), a._upload.dest = dest);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-app.post('/upload', upload.array("image"), (req, res) => {
-    res.redirect('/upload');
-    res.json({data: "success"})
-})
+app.get('/upload', (req, res) => {
+    res.send(
+        `<a href="/d/0">Download</a>`
+    );
+});
 
-app.listen(80, () => {
+app.post('/upload', (...args) => (console.log(a.upload.dest), a.upload.array("image")(...args), setTimeout(() => args[2]?.call({}), 2000)), (req, res) => {
+    currentDir ++;
+    reloadDb(__dirname + `/public/uploads/${currentDir}/`);
+    console.log(currentDir, dest);
+    res.json({success: true});
+});
+
+app.get('/d/:id', async (req, res) => {
+    try {
+        var id = req.params.id;
+        var files = await fs.readdir(__dirname + `/public/uploads${id ? `/${id}` : ''}`);
+
+        if (!files.length) {
+            return res.json({error: "No files found."});
+        }
+
+        var filename = files[0];
+
+        return res.download(__dirname + `/public/uploads/${id}/` + filename, "file" + filename.slice(filename.lastIndexOf('.')));
+    } catch {
+        return res.json({error: "File not found."})
+    }
+});
+
+app.listen(9000, () => {
     console.log('Listening for requests...');
-})
+});
